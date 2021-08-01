75cf617a59b6f6f41ad33dbb2db27e5b27d8d449
go-max07 <johnmetry17@gmail.com>
2021-08-02 01:30:22 +0300

    created instance variable for class user

--- password.py
+++ password.py
@@ -4,18 +4,27 @@ class User:
     """
     user_list =[]
 
-    def __init__(self,first_name,last_name,phone_number,email):
+    def __init__(self,first_name,last_name,password):
 
         self.first_name = first_name
         self.last_name = last_name
-        self.phone_number = phone_number
-        self.email = email
+        self.pass_word = password
+      
 
-new_contact = User("James","John","08987","james@iajwkdj.com")
+new_user = User("James","John","08987")
+
+print("First Name?")
+first_name = input()
+print("Last Name?")
+last_name = input()
+print("Password?")
+password = input()
+
+user_name = first_name+ ""+last_name
+print(user_name,password)
 
 
-
-print(new_contact.last_name,new_contact.first_name)
+print(new_user.last_name,new_user.first_name,new_user.pass_word)
 
 class Credentials:
     """
